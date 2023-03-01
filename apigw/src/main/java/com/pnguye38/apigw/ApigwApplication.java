4da87f07c76d1578d3ec76fc383b4a40bcc433bb
phuc <phuchnguyen99@gmail.com>
2023-03-01 12:05:06 -0500

    Setup Api Gateway

---
+++ apigw/src/main/java/com/pnguye38/apigw/ApigwApplication.java
@@ -0,0 +1,13 @@
+package com.pnguye38.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApigwApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApigwApplication.class, args);
+    }
+}
